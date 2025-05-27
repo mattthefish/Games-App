15d2374f68565fc98f09bc227d40ceb595dfa088
Matt Fish <matthewf@apadmi.com>
2025-05-27 12:19:52 +0100

    refactor: takes logic out of Ball, into viewmodel. Explicitly names Ball attributes in state

--- app/src/main/java/com/example/gamesApp/engine/games/brickBreaker/BrickBreakerViewModel.kt
+++ app/src/main/java/com/example/gamesApp/engine/games/brickBreaker/BrickBreakerViewModel.kt
@@ -7,10 +7,15 @@ import androidx.compose.ui.unit.Velocity
 import com.example.gamesApp.R
 import com.example.gamesApp.engine.games.GameViewModel
 import com.example.gamesApp.ui.destinations.BrickBreakerScreenDestination
+import com.example.gamesApp.ui.utils.isBetween
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import java.util.UUID
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class BrickBreakerViewModel: GameViewModel() {
     override val name: String = "Brick Breaker"
@@ -27,51 +32,185 @@ class BrickBreakerViewModel: GameViewModel() {
         )
     }
 
-    private fun updateBallOffset() {
-        updateBall(
-            offset = Offset(
-                state.value.ball.offset.x + state.value.ball.velocity.x,
-                state.value.ball.offset.y + state.value.ball.velocity.y
-            ),
-            oldBall = state.value.ball
-        )
-    }
-
-    private fun updateBall(velocity: Velocity? = null, offset: Offset? = null, oldBall: Ball) {
-        internalState.value = state.value.copy(
-            ball = Ball(
-                velocity = velocity ?: oldBall.velocity,
-                offset = offset?: oldBall.offset
-            )
-        )
-    }
-
-    private fun replaceBall(newBall: Ball){
-        internalState.value = state.value.copy(
-            ball = newBall
-        )
-    }
-
     private fun checkLaunchPadCollision(launchPadOffset: Offset, launchPadBounds: MutableState<Rect>) {
-        state.value.ball.checkRectangleCollision(
+        val hasCollided = checkRectangleCollision(
             rectBounds = launchPadBounds.value,
             isBrick = false
-        )?.let {
-            replaceBall(
-                newBall = it
-            )
-            replaceBall(
-                newBall = state.value.ball.angledVelocity(
-                    launchPadOffset = launchPadOffset,
-                    launchPadWidth = state.value.launchPad.width
-                )
+        )
+        if(hasCollided) {
+            angledVelocity(
+                launchPadOffset = launchPadOffset,
+                launchPadWidth = state.value.launchPad.width
             )
         }
-
     }
 
-    fun initOffset(x: Float, y: Float) {
-        updateBall(offset = Offset(x, y), oldBall = state.value.ball)
+    private fun angledVelocity(
+        launchPadOffset: Offset,
+        launchPadWidth: Float
+    ) {
+        val offset = state.value.ballOffset
+        val velocity = state.value.ballVelocity
+        val diameter = state.value.ball.diameter
+        val ballCenter = offset.x + diameter/2
+        val launchPadCenter = launchPadOffset.x + launchPadWidth/2
+
+        if (ballCenter.isBetween(launchPadOffset.x + launchPadWidth, launchPadOffset.x)) {
+            val relativeImpactX = (ballCenter - launchPadCenter) / (launchPadWidth / 2)
+
+            val clampedImpactX = relativeImpactX.coerceIn(-1f, 1f)
+
+            val exitAngle = Math.PI / 4 + (clampedImpactX + 1) * (Math.PI / 4)
+
+            // Compute new velocity components
+            val speed = sqrt(velocity.x.pow(2) + velocity.y.pow(2)) * -1f
+            val vx = speed * cos(exitAngle).toFloat()
+            val vy = speed * sin(exitAngle).toFloat()
+
+            setVelocityAndOffset(
+                velocity = Velocity(vx, vy)
+            )
+        }
+    }
+
+
+    private fun checkWallCollision(
+        screenWidth: Float,
+        screenHeight: Float,
+    ): Boolean {
+        val offset = state.value.ballOffset
+        val velocity = state.value.ballVelocity
+        val diameter = state.value.ball.diameter
+        var hasCollided = false
+        if (offset.x + diameter >= screenWidth
+            || offset.x <= 0
+        ) {
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    -velocity.x,
+                    velocity.y
+                )
+            )
+
+            hasCollided = if (offset.x <= 0 ) {
+                setVelocityAndOffset(offset = Offset(0f, offset.y))
+            } else {
+                setVelocityAndOffset(offset = Offset(screenWidth - diameter, offset.y))
+            }
+        } else if (offset.y + diameter >= screenHeight
+            || offset.y <= 0
+        ) {
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    velocity.x,
+                    -velocity.y
+                )
+            )
+
+            hasCollided = if (offset.y <= 0 ) {
+                setVelocityAndOffset(offset =  Offset(offset.x, 0f))
+            } else {
+                setVelocityAndOffset(offset = Offset(offset.x, screenHeight - diameter))
+            }
+        }
+        return hasCollided
+    }
+
+
+    // returns false if no collision detected
+    private fun checkRectangleCollision(
+        rectBounds: Rect,
+        isBrick: Boolean
+    ): Boolean {
+        val ballRadius = state.value.ball.diameter / 2
+        val ballX = state.value.ballOffset.x + ballRadius
+        val ballY = state.value.ballOffset.y + ballRadius
+
+        // Ball-Rectangle Collision Detection
+        val ballLeft = ballX - ballRadius
+        val ballRight = ballX + ballRadius
+        val ballTop = ballY - ballRadius
+        val ballBottom = ballY + ballRadius
+
+        val rectLeft = rectBounds.left
+        val rectRight = rectBounds.right
+        val rectTop = rectBounds.top
+        val rectBottom = rectBounds.bottom
+
+        val ballIntersectsRectHorizontally = ballRight > rectLeft && ballLeft < rectRight
+        val ballIntersectsRectVertically = ballBottom > rectTop && ballTop < rectBottom
+
+        return if (ballIntersectsRectHorizontally &&
+            ballBottom <= rectBounds.center.y &&
+            ballBottom >= rectTop &&
+            state.value.ballVelocity.y > 0
+        ) {
+            // Ball hits the top of the rectangle
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    state.value.ballVelocity.x,
+                    -state.value.ballVelocity.y
+                ),
+                offset =  Offset(state.value.ballOffset.x, rectTop - ballRadius*2)
+            )
+
+        } else if (ballIntersectsRectHorizontally &&
+            ballTop >= rectBounds.center.y &&
+            ballTop <= rectBottom &&
+            state.value.ballVelocity.y < 0 &&
+            isBrick
+        ) {
+            // Ball hits the bottom of the rectangle
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    state.value.ballVelocity.x,
+                    -state.value.ballVelocity.y
+                ),
+                offset = Offset(state.value.ballOffset.x, rectBottom)
+            )
+        } else if (ballIntersectsRectVertically &&
+            rectBounds.center.x > ballRight &&
+            ballRight > rectLeft &&
+            state.value.ballVelocity.x > 0
+        ) {
+            // Ball hits the left side of the rectangle
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    -state.value.ballVelocity.x,
+                    state.value.ballVelocity.y
+                ),
+                offset = Offset(rectLeft - ballRadius*2 - 1, state.value.ballOffset.y),
+            )
+        } else if (ballIntersectsRectVertically &&
+            rectBounds.center.x < ballLeft &&
+            ballLeft < rectRight &&
+            state.value.ballVelocity.x < 0
+        ) {
+            // Ball hits the right side of the rectangle
+            setVelocityAndOffset(
+                velocity = Velocity(
+                    -state.value.ballVelocity.x,
+                    state.value.ballVelocity.y
+                ),
+                offset = Offset(rectRight + 1f, state.value.ballOffset.y),
+            )
+        } else false
+    }
+
+    private fun updateBallOffset() {
+        val currentState = state.value
+        val newX = currentState.ballVelocity.x * 2f + currentState.ballOffset.x
+        val newY = currentState.ballVelocity.y * 2f + currentState.ballOffset.y
+
+        setVelocityAndOffset(
+            offset = Offset(newX, newY)
+        )
+    }
+
+    fun setInitOffset(x: Float, y: Float) {
+        setVelocityAndOffset(
+            offset = Offset(x, y)
+        )
     }
 
     fun onUpdate(
@@ -83,20 +222,21 @@ class BrickBreakerViewModel: GameViewModel() {
         updateBallOffset()
 
         checkLaunchPadCollision(
-                launchPadOffset = launchPadOffset,
-        launchPadBounds = launchPadBounds
+            launchPadOffset = launchPadOffset,
+            launchPadBounds = launchPadBounds
         )
 
-        state.value.ball.checkWallCollision(parentWidthPx, parentHeightPx)
+        checkWallCollision(parentWidthPx, parentHeightPx)
 
         // check brick collision
         state.value.bricks
             .filter { it.id !in state.value.destroyedBricks }
             .forEach {
-                if(state.value.ball.checkRectangleCollision(
-                        rectBounds = it.bounds,
-                        isBrick = true
-                    ) == null) {
+                val hasCollided = checkRectangleCollision(
+                    rectBounds = it.bounds,
+                    isBrick = true
+                )
+                if(!hasCollided) {
                     // no op
                 } else { // if collision happens
                     destroyBrick(it.id)
@@ -104,11 +244,33 @@ class BrickBreakerViewModel: GameViewModel() {
             }
     }
 
+    private fun setVelocityAndOffset(
+        offset: Offset? = null,
+        velocity: Velocity? = null
+    ): Boolean {
+        var changed = false
+        offset?.let {
+            internalState.value = state.value.copy(ballOffset = it)
+            changed = true
+        }
+        velocity?.let {
+            internalState.value = state.value.copy(ballVelocity = it)
+            changed = true
+        }
+        if (changed) {
+            println("internal state ball offset: ${internalState.value.ballOffset}")
+
+        }
+        return changed
+    }
+
     data class GameState(
         val paused: Boolean = false,
         val bricks: Set<Brick> = setOf(Brick(), Brick(Offset(600f, 600f)), Brick(Offset(400f, 1500f))),
         val destroyedBricks: Set<UUID> = setOf(),
         val launchPad: LaunchPad = LaunchPad(),
-        val ball: Ball = Ball(),
+        val ballVelocity: Velocity = Velocity(10f, -5f),
+        val ballOffset: Offset = Offset(250f,500f),
+        val ball: Ball = Ball()
     )
 }
--- app/src/main/java/com/example/gamesApp/ui/games/BrickBreakerScreen.kt
+++ app/src/main/java/com/example/gamesApp/ui/games/BrickBreakerScreen.kt
@@ -10,13 +10,15 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -28,8 +30,6 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.round
 import com.example.gamesApp.engine.games.brickBreaker.BrickBreakerViewModel
 import com.example.gamesApp.engine.games.brickBreaker.BrickBreakerViewModel.GameState
-import com.example.gamesApp.engine.games.brickBreaker.LaunchPad
-import com.example.gamesApp.ui.utils.collectAsStateRepeatedly
 import com.example.gamesApp.ui.utils.toDp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -41,13 +41,13 @@ fun BrickBreakerScreen(
     navigator: DestinationsNavigator
 ){
     val viewModel = BrickBreakerViewModel()
-    val state by viewModel.state.collectAsStateRepeatedly()
+    val state by viewModel.state.collectAsState()
 
     BrickBreakerScreenContent(
         state = state,
         onBackClicked = { navigator.navigateUp() },
         onUpdate = { offset, bounds, x, y -> viewModel.onUpdate(offset, bounds, x, y) },
-        initBallOffset = { x, y -> viewModel.initOffset(x, y)}
+        initBallOffset = { x, y -> viewModel.setInitOffset(x, y)}
     )
 }
 
@@ -74,27 +74,66 @@ fun BrickBreakerScreenContent(
 
         val launchPadBounds = remember { mutableStateOf(Rect.Zero) }
 
-        var launchPadOffset by remember {
+        val launchPadOffset = remember {
             mutableStateOf(
                 Offset(launchPadStartX, launchPadStartY)
             )
         }
 
-        val ballStartX = parentWidthPx / 2  - state.launchPad.width / 2
-        val ballStartY = parentHeightPx - 500f
-        initBallOffset(ballStartX, ballStartY)
+//        val ballStartX = parentWidthPx / 2  - state.launchPad.width / 2
+//        val ballStartY = parentHeightPx - 500f
+//        initBallOffset(ballStartX, ballStartY)
 
         //Change location and check collisions
         LaunchedEffect(Unit){
             while (true) {
-                onUpdate(launchPadOffset,launchPadBounds, parentWidthPx, parentHeightPx)
+                onUpdate(launchPadOffset.value,launchPadBounds, parentWidthPx, parentHeightPx)
                 delay(10L)
             }
         }
 
-        //draw bricks
-        state.bricks
-         .forEach { brick ->
+        key(state) { // Force recomposition when 'state' changes
+            Text(text = "Ball Offset: ${state.ballOffset.round()}", color = Color.White)
+        }
+
+        DrawBall(state)
+
+        DrawLaunchPad(
+            state = state,
+            launchPadBounds = launchPadBounds,
+            launchPadOffset = launchPadOffset,
+            parentWidthPx = parentWidthPx,
+            launchPadStartY = launchPadStartY
+        )
+
+        DrawBricks(state)
+    }
+}
+
+@Composable
+fun DrawBall(
+    state: GameState
+) {
+    Surface(
+        modifier = Modifier
+            .size(state.ball.diameter.toDp())
+            .offset {
+                println("Ball Offset in UI: ${state.ballOffset.round()}")
+                state.ballOffset.round()
+            }
+        ,
+        shape = CircleShape,
+        color = Color.White,
+        content = {  }
+    )
+}
+
+@Composable
+fun DrawBricks(
+    state: GameState
+) {
+    state.bricks
+        .forEach { brick ->
              AnimatedVisibility(
                  visible = brick.id !in state.destroyedBricks
              ) {
@@ -115,49 +154,43 @@ fun BrickBreakerScreenContent(
                      content = { }
                  )
              }
-         }
+        }
+}
 
-        //draw ball
-        Surface(
-            modifier = Modifier
-                .size(state.ball.diameter.toDp())
-                .offset {
-                    state.ball.offset.round()
+@Composable
+fun DrawLaunchPad(
+    state: GameState,
+    launchPadBounds:  MutableState<Rect>,
+    launchPadOffset: MutableState<Offset>,
+    parentWidthPx: Float,
+    launchPadStartY: Float
+) {
+    Surface(
+        modifier = Modifier
+            .size(
+                state.launchPad.width.toDp(),
+                state.launchPad.height.toDp()
+            )
+            .offset {
+                launchPadOffset.value.round()
+            }
+            .pointerInput(Unit) {
+                detectTransformGestures { _, pan, _, _ ->
+                    val newOffset = launchPadOffset.value + pan
+                    launchPadOffset.value = Offset(
+                        x = newOffset.x.coerceIn(
+                            0f,
+                            parentWidthPx - state.launchPad.width
+                        ),
+                        y = launchPadStartY,
+                    )
                 }
-            ,
-            shape = CircleShape,
-            color = Color.White,
-            content = {  }
-        )
-
-        //draw launch pad
-        Surface(
-            modifier = Modifier
-                .size(
-                    state.launchPad.width.toDp(),
-                    state.launchPad.height.toDp()
-                )
-                .offset {
-                    launchPadOffset.round()
-                }
-                .pointerInput(Unit) {
-                    detectTransformGestures { _, pan, _, _ ->
-                        val newOffset = launchPadOffset + pan
-                        launchPadOffset = Offset(
-                            x = newOffset.x.coerceIn(
-                                0f,
-                                parentWidthPx - state.launchPad.width
-                            ),
-                            y = launchPadStartY,
-                        )
-                    }
-                }
-                .onGloballyPositioned { coordinates ->
-                    launchPadBounds.value = coordinates.boundsInRoot()
-                },
-            shape = RectangleShape,
-            color = Color.White,
-            content = {  }
-        )
-    }
+            }
+            .onGloballyPositioned { coordinates ->
+                launchPadBounds.value = coordinates.boundsInRoot()
+            },
+        shape = RectangleShape,
+        color = Color.White,
+        content = {  }
+    )
 }
